3dc61dae1f807100ad612f3514b71dc1e56f550e
GerdHirsch <kontakt@gerdhirsch.de>
2018-04-05 17:24:16 +0200

    getStatus(future), comments

--- DemoAsync/src/main.cpp
+++ DemoAsync/src/main.cpp
@@ -11,14 +11,15 @@
 #include <iostream>
 using namespace std;
 
-//#include <chrono>
+//#include <chrono> // für s, ms, ... wird nicht benötig
 //using namespace std::literals;
 
+//---------------------------------------------------------------------
 void myTask(){
 	std::cout << __PRETTY_FUNCTION__ << std::endl;
-	std::this_thread::sleep_for(1s);
+	std::this_thread::sleep_for(1'000ms); //1'000'000 eine Million separators C++14
 }
-
+//---------------------------------------------------------------------
 void demoDeferedProblem(){
 	std::cout << __PRETTY_FUNCTION__ << std::endl;
 //	std::launch launchPolicy = std::launch::async;
@@ -30,25 +31,33 @@ void demoDeferedProblem(){
 	}
 	std::cout << "future ready!" << std::endl;
 }
+//---------------------------------------------------------------------
+template<typename ReturnType>
+std::future_status getStatus(std::future<ReturnType> const& future){
+	return future.wait_for(0s);
+}
 void demoDeferedSolution(){
 	std::cout << __PRETTY_FUNCTION__ << std::endl;
 	std::launch launchPolicy = std::launch::async;
 	auto future = std::async(launchPolicy, myTask);
-
 //	auto future = std::async(myTask); // default
 
-	if(future.wait_for(0s) == std::future_status::deferred){
+	// future.getStatus() exisitiert nicht darum
+	// ein dummy aufruf future.wait_for(0s)
+//	if(future.wait_for(0s) == std::future_status::deferred){
+
+	if(getStatus(future) == std::future_status::deferred){
 		std::cout << "future defered!" << std::endl;
 		future.wait();
 		std::cout << "future defered ready!" << std::endl;
 	} else {
 		while(future.wait_for(100ms) != std::future_status::ready){
-			std::cout << "future not ready!" << std::endl;
+			std::cout << "future async not ready!" << std::endl;
 		}
-		std::cout << "future ready!" << std::endl;
+		std::cout << "future async ready!" << std::endl;
 	}
 }
-
+//---------------------------------------------------------------------
 template<typename Task, typename... Args>
 inline
 auto // C++14 return type deduction
@@ -62,6 +71,7 @@ reallyAsync(Task&& task, Args&&... args)
 					std::forward<Args>(args)...
 			);
 }
+
 void demoReallyAsync(){
 	std::cout << __PRETTY_FUNCTION__ << std::endl;
 	auto future = reallyAsync(myTask);
@@ -76,8 +86,8 @@ void demoReallyAsync(){
 		}
 		std::cout << "future ready!" << std::endl;
 	}
-
 }
+//---------------------------------------------------------------------
 int main(){
 	cout << "DemoAsync" << endl;
 //	demoDeferedProblem();
