611b74ddcee511b450ce77da638e5a1e739e88d0
GerdHirsch <kontakt@gerdhirsch.de>
2018-04-09 11:37:16 +0200

    project options c++1y

--- StaleData/src/main.cpp
+++ StaleData/src/main.cpp
@@ -1,14 +1,17 @@
-#include <iostream>
+#include <future>
 #include <thread>
+#include <iostream>
 #include <mutex>
 #include <atomic>
 
 using namespace std;
 
-bool sleep = false;
+//#include <chrono> // für s, ms, ... wird nicht benötig
+//using namespace std::chrono_literals;
 
-bool ready{false};
+bool sleep = false;
 int number{-1};
+//---------------------------------------------------------------------
 std::atomic<bool> readyAtomic{false};
 
 //static std::memory_order mo_load = std::memory_order_acquire;
@@ -17,6 +20,7 @@ static std::memory_order mo_load = std::memory_order_relaxed;
 static std::memory_order mo_store = std::memory_order_relaxed;
 
 void taskAtomicFunction(){
+	cout << __PRETTY_FUNCTION__ << endl;
 	int count = 0;
 	while( !readyAtomic.load(mo_load) ) {
 //		std::this_thread::yield();
@@ -26,18 +30,22 @@ void taskAtomicFunction(){
 	cout << "number: " << number << endl;
 }
 void demoAtomic(){
-	cout << "demoAtomic()" << endl;
+	cout << __PRETTY_FUNCTION__ << endl;
 	thread t1(taskAtomicFunction);
 
 	number = 42;
 
-	if(sleep) std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+	if(sleep) std::this_thread::sleep_for(1'000ms);
+//	if(sleep) std::this_thread::sleep_for(std::chrono::nanoseconds(1));
 
 	readyAtomic.store(true, mo_store);
 	t1.join();
 }
+//---------------------------------------------------------------------
+bool ready{false};
 
 void taskNonAtomicFunction(){
+	cout << __PRETTY_FUNCTION__ << endl;
 	int count = 0;
 	while( !ready ) {
 //		std::this_thread::yield();
@@ -48,7 +56,7 @@ void taskNonAtomicFunction(){
 }
 
 void demoNonAtomic(){
-	cout << "demoNonAtomic()" << endl;
+	cout << __PRETTY_FUNCTION__ << endl;
 	thread t1(taskNonAtomicFunction);
 
 	number = 42;
@@ -58,10 +66,9 @@ void demoNonAtomic(){
 	ready = true;
 	t1.join();
 }
+//---------------------------------------------------------------------
 int main(){
-
 	cout << "StaleData" << endl;
 	demoNonAtomic();
 	demoAtomic();
-
 }
