6345c31f821103f46e47d140cd3b85257981d924
GerdHirsch <kontakt@gerdhirsch.de>
2018-04-11 12:54:46 +0200

    Lock/NoLocked cout, code reorganisation

--- ActivateAnotherTask/src/DemoPromiseFuture.cpp
+++ ActivateAnotherTask/src/DemoPromiseFuture.cpp
@@ -52,40 +52,58 @@ void demoPromiseFuture(){
 	t.join();
 }
 
+template<typename Mutex>
+struct NoLock{
+	NoLock(Mutex& m){}
+};
 
 void demoPromiseFutureMultipleThreads(){
-	cout << __PRETTY_FUNCTION__ << "thread id: " << this_thread::get_id() << endl;
+	bool activateBeforeWait = false;
+//	using Lock = std::unique_lock<std::mutex>;
+	using Lock = NoLock<std::mutex>;
+	std::mutex coutMutex;
+
+
+	cout << boolalpha;
+	cout << __PRETTY_FUNCTION__ << " thread id: " << this_thread::get_id() << endl;
+	cout << "activate Before Wait: " << activateBeforeWait << endl;
 
 	auto duration = 100ms;
-	bool notifyBeforeWait = false;
 
 	std::promise<void> p;
 	using Future = decltype(p.get_future().share());
-	auto lambda = [duration](Future f){
+	auto lambda = [&coutMutex, duration](Future f){
 
 		this_thread::sleep_for(duration);
-
-		cout << "before f.wait() " << __PRETTY_FUNCTION__ << "thread id: " << this_thread::get_id() << endl;
+		{
+			Lock lk(coutMutex);
+			cout << "before f.wait() " << "thread id: " << this_thread::get_id() << " " << __PRETTY_FUNCTION__  << endl;
+		}
 		f.wait(); // #1
-		cout << "after f.wait() " << endl;
+
+		Lock lk(coutMutex);
+		cout << "after f.wait() " << " thread id: " << this_thread::get_id() << endl;
 		};
 
 	auto sf = p.get_future().share();
 
-	constexpr int numThreads = 3;
+	constexpr int numThreads = 4;
 	vector<thread> threads;
 	for(int i=0; i<numThreads; ++i)
 		threads.push_back(thread(lambda, sf));
 //		threads.emplace(lambda, sf);
 
-	if(notifyBeforeWait)
+	if(activateBeforeWait)
 		this_thread::sleep_for(duration - 100ms); // notifiy before wait
 	else
 		this_thread::sleep_for(duration + 100ms); // notifiy after wait
 
 	cout << "before p.set_value() " << endl;
 	p.set_value(); // #2
-	cout << "after p.set_value() " << endl;
+	{
+		Lock lk(coutMutex);
+		cout << "after p.set_value() " << endl;
+	}
 
 	for(int i=0; i<numThreads; ++i)
 		threads[i].join();
